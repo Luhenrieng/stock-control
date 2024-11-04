fb89cae42aeed7327d8b436846f55cae0ba48d5f
Luiz Henrique de Oliveira <luizhenri22@hotmail.com>
2024-11-03 23:05:08 -0300

    Login and Signup forms completed

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  loginCard = true;
 
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder){}
+
+  OnSubmitLoginForm(): void{
+    console.log("Dados do formulário de login", this.loginForm.value);
+  }
+
+  OnSubmitSignupForm(): void{
+    console.log("Dados do formulário de criação de conta", this.signupForm.value);
+  }
 }
