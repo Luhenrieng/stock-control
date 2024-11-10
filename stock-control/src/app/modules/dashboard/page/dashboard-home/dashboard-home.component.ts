de642c7e5bd86081edd5c85a2e996def35293f50
Luiz Henrique de Oliveira <luizhenri22@hotmail.com>
2024-11-09 23:12:21 -0300

    Add chart from chart.js

--- stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,3 +1,4 @@
+import { ChartData, ChartOptions } from 'chart.js';
 import { ProductsDataTransferService } from './../../../../shared/services/products/products-data-transfer.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
@@ -11,9 +12,10 @@ import { ProductsService } from 'src/app/services/products/products.service';
   styleUrls: [],
 })
 export class DashboardHomeComponent implements OnInit, OnDestroy {
-
   private destroy$ = new Subject<void>();
   public productsList: Array<GetAllProductsResponse> = [];
+  public productsChartDatas!: ChartData;
+  public productsChartOptions!: ChartOptions;
 
   constructor(
     private productsService: ProductsService,
@@ -26,25 +28,85 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
   }
 
   getProductsData(): void {
-    this.productsService.getAllProducts()
-    .pipe(takeUntil(this.destroy$))
-    .subscribe({
-      next: (response) => {
-        if (response.length > 0) {
-          this.productsList = response;
-          this.productsDtService.setProductsData(this.productsList);
-        }
-      },
-      error: (err) => {
-        console.log(err);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Erro',
-          detail: 'Erro ao buscar produtos',
-          life: 2500,
-        });
-      },
-    });
+    this.productsService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsList = response;
+            this.productsDtService.setProductsData(this.productsList);
+            this.setProductsChartConfig();
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500,
+          });
+        },
+      });
+  }
+
+  setProductsChartConfig(): void {
+    if (this.productsList.length > 0) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue(
+        '--text-color-secondary'
+      );
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      this.productsChartDatas = {
+        labels: this.productsList.map((element) => element!.name),
+        datasets: [
+          {
+            label: 'Quantidade',
+            backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+            borderColor: documentStyle.getPropertyValue('--indigo-400'),
+            hoverBackgroundColor:
+              documentStyle.getPropertyValue('--indigo-500'),
+            data: this.productsList.map((element) => element!.amount),
+          },
+        ],
+      };
+
+      this.productsChartOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: 500,
+              },
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+    }
   }
 
   ngOnDestroy(): void {
